3d4023e69bfbcbb6a3b1079f9d3c5a67c3dcc7cb
brunasvicente <ra56515691890@acaonsfatima.org.br>
2024-08-12 18:24:18 -0300

    Exercícios 5 e 6 concluídos

--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './pages/app';
 import Sobre from './pages/sobre';
-import Exercicioum from './pages/exercicioum';
-import Exerciciodois from './pages/exerciciodois';
-import Exerciciotres from './pages/exerciciotres';
+import Exercicio1 from './pages/exercicio1';
+import Exercicio2 from './pages/exercicio2';
+import Exercicio3 from './pages/exercicio3';
+import Exercicio4 from './pages/exercicio4';
+import Exercicio5 from './pages/exercicio5';
+import Exercicio6 from './pages/exercicio6';
 
 export default function Navegacao() {
     return(
@@ -12,9 +15,12 @@ export default function Navegacao() {
           <Routes>
             <Route path='/' element={ <App/> }/>
             <Route path='/sobre' element={ <Sobre/> }/>
-            <Route path='/exercicioum' element={ <Exercicioum/> }/>
-            <Route path='/exerciciodois' element={ <Exerciciodois/> }/>
-            <Route path='/exerciciotres' element={ <Exerciciotres/> }/>
+            <Route path='/exercicio1' element={ <Exercicio1/> }/>
+            <Route path='/exercicio2' element={ <Exercicio2/> }/>
+            <Route path='/exercicio3' element={ <Exercicio3/> }/>
+            <Route path='/exercicio4' element={ <Exercicio4/> }/>
+            <Route path='/exercicio5' element={ <Exercicio5/> }/>
+            <Route path='/exercicio6' element={ <Exercicio6/> }/>
           </Routes>
         </BrowserRouter>     
     )
